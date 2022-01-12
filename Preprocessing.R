364862a476aff780ffa477a3f20500f2fb283759
sophi890 <48591088+sophi890@users.noreply.github.com>
2022-01-12 13:41:51 +1300

    Updating preprocessing

--- Preprocessing.R
+++ Preprocessing.R
@@ -9,20 +9,33 @@ library(Hmisc)
 temp = tempfile()
 download.file('https://www2.census.gov/programs-surveys/acs/data/pums/2019/5-Year/csv_pus.zip', temp)
 husa = read.csv(unz(temp, 'psam_husa.csv'))
+husb = read.csv(unz(temp, 'psam_husb.csv'))
+husc = read.csv(unz(temp, 'psam_husc.csv'))
+husd = read.csv(unz(temp, 'psam_husd.csv'))
 unlink(temp)
-
-husa = read.csv('data/psam_husa.csv')
-husb = read.csv('data/psam_husb.csv')
-husc = read.csv('data/psam_husc.csv')
-husd = read.csv('data/psam_husd.csv')
 hus = rbind(husa, husb, husc, husd)
 
+#husa = read.csv('data/psam_husa.csv')
+#husb = read.csv('data/psam_husb.csv')
+#husc = read.csv('data/psam_husc.csv')
+#husd = read.csv('data/psam_husd.csv')
+#hus = rbind(husa, husb, husc, husd)
+
 # Read in person PUMS
-pusa = read.csv('data/psam_pusa.csv')
-pusb = read.csv('data/psam_pusb.csv')
-pusc = read.csv('data/psam_pusc.csv')
-pusd = read.csv('data/psam_pusd.csv')
+temp = tempfile()
+download.file('https://www2.census.gov/programs-surveys/acs/data/pums/2019/5-Year/csv_pus.zip', temp)
+pusa = read.csv(unz(temp, 'psam_pusa.csv'))
+pusb = read.csv(unz(temp, 'psam_pusb.csv'))
+pusc = read.csv(unz(temp, 'psam_pusc.csv'))
+pusd = read.csv(unz(temp, 'psam_pusd.csv'))
+unlink(temp)
 pus = rbind(pusa, pusb, pusc, pusd)
+
+#pusa = read.csv('data/psam_pusa.csv')
+#pusb = read.csv('data/psam_pusb.csv')
+#pusc = read.csv('data/psam_pusc.csv')
+#pusd = read.csv('data/psam_pusd.csv')
+#pus = rbind(pusa, pusb, pusc, pusd)
 
 # Merge
 pus.hus.merged = merge(pus[,c('PUMA', 'ST', 'SERIALNO', 'AGEP', 'SEX', 'RAC1P', 'POVPIP', 'SCHL', 'PWGTP')], hus[,c('PUMA', 'ST', 'SERIALNO', 'TEN', 'WGTP')], by = c('PUMA', 'ST', 'SERIALNO'), all.x = T)
@@ -62,8 +75,6 @@ pus.hus.merged$owner_occupied.factor = as.factor(ifelse(pus.hus.merged$TEN <= 2,
 equivfilePaths <- list.files('data/pums_equivalencies', "\\.txt$", full.names = TRUE)
 equiv <- do.call("rbind", lapply(equivfilePaths, function(x){read.delim(x, stringsAsFactor = FALSE, header = F)}))
 
-# example: 796 34 017797950 0101 001 00882270  
-# example: 796 01 017797750 0100 033 00161542
 # example: 796 11 017023820 0101 001 01702382
 # format:  796 StateFIPS StateNationalStandardCode PUMAcode CountyFIPS CountyNationalStandardCode
 
@@ -97,7 +108,7 @@ covid_us_hist <- subset(covid_hist, Country_Region == "US" & is.na(FIPS) == F)
 
 # Import outcome data from JHU CSSE
 covid <- read.csv(text = getURL(paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/", date_of_study, ".csv")))
-covid_us <- subset(covid, Country.Region == "US") # change here country_region and [,1:12]
+covid_us <- subset(covid, Country_Region == "US")[, 1:12]
 covid_us <- rbind(covid_us, subset(covid_us_hist, (!(FIPS %in% covid_us$FIPS)) & Confirmed == 0 & Deaths == 0 & is.na(FIPS) == F))
 covid_us$FIPS <- str_pad(covid_us$FIPS, 5, pad = "0")
 
@@ -112,55 +123,8 @@ county_brfss <- county_brfss[, c('fipscode', 'v011_rawvalue', 'v009_rawvalue')]
 names(county_brfss) <- c('fips', 'obese', 'smoke')
 county_brfss$fips <- str_pad(county_brfss$fips, 5, pad = "0")
 
-state_test <- read.csv(text = getURL("https://api.covidtracking.com/v1/states/daily.csv"))
-state_test <- subset(state_test, date == paste0(substring(str_remove_all(date_of_study, "-"), 5, 8),substring(str_remove_all(date_of_study, "-"), 1, 4)))[, - 38]
-statecode <- read.csv(text = getURL("https://raw.githubusercontent.com/wxwx1993/PM_COVID/master/Data/statecode.csv"))
-
 hospitals <- read.csv(text = getURL("https://opendata.arcgis.com/datasets/6ac5e325468c4cb9b905f1728d6fbf0f_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D"))
 hospitals$BEDS[hospitals$BEDS < 0] <- NA
-
-county_base_mortality <- read.table(text = getURL("https://raw.githubusercontent.com/wxwx1993/PM_COVID/master/Data/county_base_mortality.txt"), sep = "", header = TRUE)
-county_old_mortality <- read.table(text = getURL("https://raw.githubusercontent.com/wxwx1993/PM_COVID/master/Data/county_old_mortality.txt"), sep = "", header = TRUE)
-county_014_mortality <- read.table("https://raw.githubusercontent.com/wxwx1993/PM_COVID/master/Data/county_014_mortality.txt", sep = "", header = TRUE)
-county_1544_mortality <- read.table("https://raw.githubusercontent.com/wxwx1993/PM_COVID/master/Data/county_1544_mortality.txt", sep = "", header = TRUE)
-county_4564_mortality <- read.table("https://raw.githubusercontent.com/wxwx1993/PM_COVID/master/Data/county_4564_mortality.txt", sep = "", header = TRUE)
-
-colnames(county_old_mortality)[4] <- c("older_Population")
-colnames(county_014_mortality)[4] <- c("014_Population")
-colnames(county_1544_mortality)[4] <- c("1544_Population")
-colnames(county_4564_mortality)[4] <- c("4564_Population")
-
-county_base_mortality <- merge(county_base_mortality,county_old_mortality[, c(2, 4)], by = "County.Code", all.x = TRUE)
-county_base_mortality <- merge(county_base_mortality,county_014_mortality[, c(2, 4)], by = "County.Code", all.x = TRUE)
-county_base_mortality <- merge(county_base_mortality,county_1544_mortality[, c(2, 4)], by = "County.Code", all.x = TRUE)
-county_base_mortality <- merge(county_base_mortality,county_4564_mortality[, c(2, 4)], by = "County.Code", all.x = TRUE)
-
-county_base_mortality$older_pecent <- county_base_mortality$older_Population / county_base_mortality$Population
-county_base_mortality$"young_pecent" <- county_base_mortality$"014_Population" / county_base_mortality$Population
-county_base_mortality$"prime_pecent" <- county_base_mortality$"1544_Population" / county_base_mortality$Population
-county_base_mortality$"mid_pecent" <- county_base_mortality$"4564_Population" / county_base_mortality$Population
-county_base_mortality$"older_pecent"[is.na(county_base_mortality$"older_pecent")] <- 0
-county_base_mortality$"prime_pecent"[is.na(county_base_mortality$"prime_pecent")] <- 0
-county_base_mortality$"mid_pecent"[is.na(county_base_mortality$"mid_pecent")] <- 0
-county_base_mortality$"young_pecent"[is.na(county_base_mortality$"young_pecent")] <- 0
-
-# Import NCHS Urban-Rural Classification Scheme for Counties
-NCHSURCodes2013 <- read.csv("https://raw.githubusercontent.com/wxwx1993/PM_COVID/master/Data/NCHSURCodes2013.csv")
-NCHSURCodes2013$FIPS <- str_pad(NCHSURCodes2013$FIPS, 5, pad = "0")
-
-# Import FB survey on covid-like sympton data
-script <- getURL("https://raw.githubusercontent.com/cmu-delphi/delphi-epidata/main/src/client/delphi_epidata.R", ssl.verifypeer = FALSE)
-eval(parse(text = script))
-
-# Import social distancing measure data
-state_policy <- read.csv("https://raw.githubusercontent.com/wxwx1993/PM_COVID/master/Data/state_policy0410.csv")
-colnames(state_policy)[6] <- "stay_at_home"
-
-# merging data
-state_test <- merge(state_test, statecode, by.x = "state" , by.y = "Code")
-state_test <- merge(state_test, state_policy[, c(1, 6)], by = "State")
-state_test$date_since_social <- as.numeric(as.Date(Sys.Date()) - as.Date((strptime(state_test$stay_at_home, "%m/%d/%Y"))))
-state_test[is.na(state_test$date_since_social) == TRUE, ]$date_since_social <- 0
 
 # pm2.5 average over 17 years
 county_pm_aggregated <- county_pm %>% 
@@ -213,39 +177,14 @@ aggregate_pm_census <- merge(aggregate_pm,
                              by.x = "fips",
                              by.y = "fips")
 
-county_base_mortality$County.Code <- str_pad(county_base_mortality$County.Code, 5, pad = "0")
-aggregate_pm_census_cdc <- merge(aggregate_pm_census,
-                                 county_base_mortality[, c("County.Code", 
-                                                           "Population",
-                                                           "older_pecent",
-                                                           "young_pecent",
-                                                           "prime_pecent",
-                                                           "mid_pecent")],
-                                 by.x = "fips",
-                                 by.y = "County.Code",
-                                 all.x = TRUE)
 
-aggregate_pm_census_cdc <- aggregate_pm_census_cdc[is.na(aggregate_pm_census_cdc$fips) == F, ]
+aggregate_pm_census_cdc <- aggregate_pm_census[is.na(aggregate_pm_census$fips) == F, ]
 
-aggregate_pm_census_cdc_test <- merge(aggregate_pm_census_cdc,
-                                      state_test[, !(names(state_test) %in% c("fips"))],
-                                      by.x = "Province_State",
-                                      by.y = "State")
-
-aggregate_pm_census_cdc_test_beds <- merge(aggregate_pm_census_cdc_test,county_hospitals_aggregated,
+aggregate_pm_census_cdc_test_beds <- merge(aggregate_pm_census_cdc,county_hospitals_aggregated,
                                            by.x = "fips",
                                            by.y = "COUNTYFIPS",
                                            all.x = TRUE)
 aggregate_pm_census_cdc_test_beds$beds[is.na(aggregate_pm_census_cdc_test_beds$beds)] <- 0
-
-
-
-
-aggregate_pm_census_cdc_test_beds <- merge(aggregate_pm_census_cdc_test_beds,
-                                           NCHSURCodes2013[, c(1, 7)],
-                                           by.x = "fips",
-                                           by.y = "FIPS", 
-                                           all.x = TRUE)
 
 # Combine five boroughs of NYC
 aggregate_pm_census_cdc_test_beds[aggregate_pm_census_cdc_test_beds$Admin2 == "New York City", ]$population <-
@@ -261,9 +200,7 @@ aggregate_pm_census_cdc_test_beds[aggregate_pm_census_cdc_test_beds$Admin2 == "N
   subset(aggregate_pm_census_cdc_test_beds, Admin2 == "Queens" & Province_State == "New York")$beds +
   subset(aggregate_pm_census_cdc_test_beds, Admin2 == "Richmond" & Province_State == "New York")$beds
 
-vars <- c("mean_pm25", "poverty", "medianhousevalue", "medhouseholdincome", "pct_owner_occ",
-          "education", "pct_blk", "hispanic", "older_pecent", "prime_pecent", "mid_pecent", "obese", "smoke",
-          "mean_summer_temp", "mean_summer_rm", "mean_winter_temp", "mean_winter_rm")
+vars <- c("mean_pm25", "obese", "smoke","mean_summer_temp", "mean_summer_rm", "mean_winter_temp", "mean_winter_rm")
 aggregate_pm_census_cdc_test_beds[aggregate_pm_census_cdc_test_beds$Admin2 == "New York City",][, vars] <-
   sapply(vars, function(var) {
     (subset(aggregate_pm_census_cdc_test_beds, Admin2=="New York City" & Province_State=="New York")[, var] *
@@ -290,8 +227,8 @@ aggregate_pm_census_cdc_test_beds <- subset(aggregate_pm_census_cdc_test_beds,
                                               !(Admin2 == "Richmond" & Province_State == "New York"))
 
 # Merge the dataframe with historical averages of O3 and NO2
-covid_data<-data.frame(read.csv('data/covid_data20210202_race3.csv'))
-covid_data = merge(cbind.data.frame(fips = covid_data$fips, state = covid_data$State, mean_no2 = covid_data$mean_no2, mean_ozone = covid_data$mean_ozone),
+data<-read.csv('data/county_no2_ozone.csv')
+covid_data = merge(cbind.data.frame(fips = data$fips, state = data$state, mean_no2 = data$mean_no2, mean_ozone = data$mean_ozone),
                    cbind(fips = as.numeric(aggregate_pm_census_cdc_test_beds$fips), Deaths = aggregate_pm_census_cdc_test_beds$Deaths, population = aggregate_pm_census_cdc_test_beds$population, 
                          mean_pm25 = aggregate_pm_census_cdc_test_beds$mean_pm25, q_popdensity = aggregate_pm_census_cdc_test_beds$q_popdensity, 
                           beds = aggregate_pm_census_cdc_test_beds$beds, 
@@ -304,8 +241,7 @@ censustract.pm25$geoid = str_pad(censustract.pm25$geoid, 11, pad = '0')
 censustract.pm25$fips = paste(substring(censustract.pm25$geoid, 1, 2), substring(censustract.pm25$geoid, 3,5), sep = "")
 
 # read in census 2018 data which has population by census tract
-censusdata = read.csv('data/census_tract_2009_2019.csv')
-census2018 = subset(censusdata, year == '2018')
+census2018 = read.csv('data/census2018.csv')
 
 # merge with population data from census2018
 censustract.pm25 = merge(censustract.pm25, cbind(geoid = census2018$geoid, population = census2018$population), by = 'geoid', all.x = T)
@@ -316,23 +252,12 @@ weightedvarlist.pm25 = sapply(split(censustract.pm25, censustract.pm25$fips), fu
 # Merge weighted pm25 variance list with previous dataset to ensure same order of FIPS
 covid_data = merge(as.data.frame(cbind(fips = as.numeric(names(weightedvarlist.pm25)), weightedvarlist.pm25)), covid_data, by = 'fips')
 
-# Create list of 3 x 3 covariance matrices for log household income, log house value, PM2.5 (assume PM2.5 is uncorrelated with the previous two)
-covlist.pm25 = vector(mode = "list", length = length(covid_data$fips))
-for (i in 1:length(covlist)){
-  covlist.pm25[[i]] = matrix(0, nrow = 3, ncol = 3)
-  covlist.pm25[[i]][1:2,1:2] = covlist[[i]]
-  covlist.pm25[[i]][3,3] = covid_data$weightedvarlist.pm25[i]
-}
-save(covlist.pm25, file = 'covlist.pm25.Rda')
-
 # FINANCIAL VARIABLES : extract variance and means
 
 hus$HINCP[which(hus$HINCP <= 0)] = NA # remove negative incomes. These are only 1 percent of households
 
 ## Updated with weights
 covlist = vector(mode = "list", length = length(covid_data$fips))
-loghouseholdincome = rep(NA, length(covid_data$fips))
-loghousevalue = rep(NA, length(covid_data$fips))
 ## Create list of matrices, length of fips (each list item is the 2 x 2 covariance matrix for that fips)
 for (i in 1:length(covid_data$fips)){
   fip = str_pad(covid_data$fips[i], 5, pad = '0')
@@ -340,12 +265,17 @@ for (i in 1:length(covid_data$fips)){
   data = na.omit(as.data.frame(cbind(HINCP = fipdat$HINCP, ADJINC = fipdat$ADJINC, VALP = fipdat$VALP, WGTP = fipdat$WGTP)))
   covmat = cov.wt(x = cbind(log((data$ADJINC/1000000)*(data$HINCP)), log(data$VALP)), wt = data$WGTP)# inflation factor ADJINC
   covlist[[i]] = covmat$cov
-  loghouseholdincome[i] = covmat$center[1]
-  loghousevalue[i] = covmat$center[2]
+  covid_data$loghouseholdincome[i] = covmat$center[1]
+  covid_data$loghousevalue[i] = covmat$center[2]
 }
 
-covid_data$loghouseholdincome = loghouseholdincome
-covid_data$loghousevalue = loghousevalue
+# Create list of 3 x 3 covariance matrices for log household income, log house value, PM2.5 (assume PM2.5 is uncorrelated with the previous two)
+covlist.pm25 = vector(mode = "list", length = length(covid_data$fips))
+for (i in 1:length(covlist)){
+  covlist.pm25[[i]] = matrix(0, nrow = 3, ncol = 3)
+  covlist.pm25[[i]][1:2,1:2] = covlist[[i]]
+  covlist.pm25[[i]][3,3] = covid_data$weightedvarlist.pm25[i]
+}
 
 # Create joint distribution of age, sex, race, poverty, education, owner occupied: matrix called cross (3082 x # combs)
 # 2 different options depending on how many categories age is (2 or 4, leads to 96 or 192 strata).
