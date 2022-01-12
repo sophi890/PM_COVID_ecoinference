93004fa55e686492ae9f366094eb03da8ab23f72
sophi890 <48591088+sophi890@users.noreply.github.com>
2022-01-12 17:40:08 +1300

    started

--- print_results.R
+++ print_results.R
@@ -2,14 +2,9 @@ library('rstan')
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
-load('results/fit_withoutsex.Rdata')
-print(fit_withoutsex)
-for (i in 1:23){
-  print(noquote(paste(signif(exp(mean(extract(fit_withoutsex)$pars[,i])),4), ' & ','(', signif(exp(mean(extract(fit_withoutsex)$pars[,i])-1.96*sd(extract(fit_withoutsex)$pars[,i])),4), ', ', signif(exp(mean(extract(fit_withoutsex)$pars[,i])+1.96*sd(extract(fit_withoutsex)$pars[,i])),4), ')', sep = '')))
-}
-
-load('results/fit_sexonly.Rdata')
-print(fit_sexonly)
-for (i in 1:23){
-  print(noquote(paste(signif(exp(mean(extract(fit_withoutsex)$pars[,i])),4), ' & ','(', signif(exp(mean(extract(fit_withoutsex)$pars[,i])-1.96*sd(extract(fit_withoutsex)$pars[,i])),4), ', ', signif(exp(mean(extract(fit_withoutsex)$pars[,i])+1.96*sd(extract(fit_withoutsex)$pars[,i])),4), ')', sep = '')))
+# Main Analysis
+load('results/fit6.Rdata')
+print(fit6)
+for (i in 1:24){
+  print(noquote(paste(signif(exp(mean(extract(fit6)$pars[,i])),4), ' & ','(', signif(exp(mean(extract(fit6)$pars[,i])-1.96*sd(extract(fit6)$pars[,i])),4), ', ', signif(exp(mean(extract(fit6)$pars[,i])+1.96*sd(extract(fit6)$pars[,i])),4), ')', sep = '')))
 }
