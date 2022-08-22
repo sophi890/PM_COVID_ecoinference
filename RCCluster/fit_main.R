682cc5f42381cacaa2cd313febdccbd5072e58d3
sophi890 <48591088+sophi890@users.noreply.github.com>
2022-08-22 15:21:06 -0400

    update w strong fem/age priors

--- RCCluster/fit_main.R
+++ RCCluster/fit_main.R
@@ -4,15 +4,18 @@ rstan_options(auto_write = TRUE)
 
 load("../ecoreg_main.RData")
 
-fit_main = stan(file = '../ecoreg_random.stan', 
-            data = list(y=adata[,1], 
-                        numcounties = 3082, 
-                        numeffects = c(14, 6, 3), 
-                        numcats = c(2,2,2,2,2,3), 
-                        covlist=covlist.pm25, 
-                        adata=adata, 
-                        whicha=whicha, 
-                        states=states), iter = 6000)
+fit_main = stan(file = '../ecoreg_main.stan', 
+                                 data = list(y=adata[,1], 
+                                             numcounties = 3082, 
+                                             numeffects = c(13, 6, 3), 
+                                             numcats = c(2,2,2,2,2,3), 
+                                             covlist=covlist.pm25, 
+                                             adata=adata, 
+                                             whicha=whicha, 
+                                             states=states), 
+                         iter = 4000)
 
 
 save(fit_main, file=paste0("../results/fit_main.Rdata"))
+
+
--- RCCluster/fit_main.sh
+++ RCCluster/fit_main.sh
@@ -2,8 +2,9 @@
 
 #SBATCH -n 4 #number of cores
 #SBATCH -t 6-23:59 # max time limit on this queue is 7 days
-#SBATCH -o /n/home07/swoodward/PM_COVID_ecoinference/RCCluster/test_error.out #specify where to save errors returned by the program
-#SBATCH -e /n/home07/swoodward/PM_COVID_ecoinference/RCCluster/test_log.err #specify where to save the output log
+#SBATCH -p shared #partition
+#SBATCH -o /n/home07/swoodward/PM_COVID_ecoinference/RCCluster/main_error.out #specify where to save errors returned by the program
+#SBATCH -e /n/home07/swoodward/PM_COVID_ecoinference/RCCluster/main_log.err #specify where to save the output log
 #SBATCH --array=1 
 #SBATCH --mem=8000 #memory requested
 #SBATCH -J fit_main  #job name
@@ -16,4 +17,4 @@ module load gcc/9.2.0-fasrc01 R/3.6.3-fasrc02
 #module load R_core/3.4.2-fasrc01
 #module load R_packages/3.4.2-fasrc02
 
-R CMD BATCH --quiet --no-restore --no-save fit_main.R /n/home07/swoodward/PM_COVID_ecoinference/RCCluster/test_${SLURM_ARRAY_TASK_ID}.Rout
+R CMD BATCH --quiet --no-restore --no-save fit_main.R /n/home07/swoodward/PM_COVID_ecoinference/RCCluster/main_${SLURM_ARRAY_TASK_ID}.Rout
